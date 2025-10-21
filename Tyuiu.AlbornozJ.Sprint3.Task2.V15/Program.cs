05e6d52d612b658f6ce4d5c26e2aa29b842adcd9
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-21 22:13:06 +0500

    Task2

---
+++ Tyuiu.AlbornozJ.Sprint3.Task2.V15/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task2.V15.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+double value = 0.5;
+int startValue = 1;
+int stopValue = 20;
+
+Console.WriteLine("Переменная a = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+Console.ReadKey();
