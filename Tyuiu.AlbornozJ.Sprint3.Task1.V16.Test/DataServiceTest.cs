f894aff7f8c17c1344fdc6fc0f56c040f026c7e9
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-21 21:47:19 +0500

    Task1

---
+++ Tyuiu.AlbornozJ.Sprint3.Task1.V16.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿
+using Tyuiu.AlbornozJ.Sprint3.Task1.V16.Lib;
+
+namespace Tyuiu.AlbornozJ.Sprint3.Task1.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.7;
+            int startValue = 1;
+            int stopValue = 15;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 15.948;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint3.Task1.V16/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task1.V16.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+double value = 0.7;
+int startValue = 1;
+int stopValue = 15;
+
+Console.WriteLine("Переменная X = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine("Сумма ряда = " + ds.GetSumSeries(value, startValue, stopValue));
+Console.ReadKey();
