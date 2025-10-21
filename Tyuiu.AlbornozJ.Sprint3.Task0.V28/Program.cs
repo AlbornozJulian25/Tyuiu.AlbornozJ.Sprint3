c2a6adf81937c56410737ec7dd807bfab4498f1e
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-21 21:18:24 +0500

    Add project files.

---
+++ Tyuiu.AlbornozJ.Sprint3.Task0.V28/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task0.V28.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+
+double value = 0.25;
+int startValue = 1;
+int stopValue = 17;
+
+Console.WriteLine("Переменная X = " + value);
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+Console.ReadKey();
