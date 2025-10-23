98dfa314d7efd8c3de54980c26b23611744ea6a4
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-24 00:00:17 +0500

    Task3

---
+++ Tyuiu.AlbornozJ.Sprint3.Task3.V17.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AlbornozJ.Sprint3.Task3.V17.Lib
+{
+    public class DataService : ISprint3Task3V17
+    {
+        public int ConvertStringToInt(string value)
+        {
+            string numbersOnly = "";
+
+            foreach (char chr in value)
+            {
+                if (chr >= '0' && chr <= '9')
+                {
+                    numbersOnly += chr;
+                }
+            }
+
+            return int.Parse(numbersOnly);
+        }
+    }
+}
---
+++ Tyuiu.AlbornozJ.Sprint3.Task3.V17/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task3.V17.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+string value = "*vn98n! b";
+
+Console.WriteLine("Исходная строка = " + value);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Полученное число = " + ds.ConvertStringToInt(value));
+Console.ReadKey();
