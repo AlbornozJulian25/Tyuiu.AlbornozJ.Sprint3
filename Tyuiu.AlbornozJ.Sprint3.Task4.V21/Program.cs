2e44ed88145fa8bc7e1954349ea52db37c28a1a0
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-25 23:13:45 +0500

    Task4

---
+++ Tyuiu.AlbornozJ.Sprint3.Task4.V21/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task4.V21.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Произведение ряда = " + ds.Calculate(startValue, stopValue));
+Console.ReadKey();
