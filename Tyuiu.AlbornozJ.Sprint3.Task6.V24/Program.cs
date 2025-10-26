7e33cb6fd7bf4f8a3638b0993e5c27c469dfc2f0
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-26 22:38:30 +0500

    Task6

---
+++ Tyuiu.AlbornozJ.Sprint3.Task6.V24/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task6.V24.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = 17;
+int stopValue = 26;
+
+Console.WriteLine("Начало отрезка = " + startValue);
+Console.WriteLine("Конец отрезка = " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Количество всех делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+Console.ReadKey();
