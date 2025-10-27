9e11a06a2a355033a0a4e146da798a125d074d29
Julián Albornoz García <Tyuiu.AlbornozJ@outlook.com>
2025-10-27 16:40:30 +0500

    Task7

---
+++ Tyuiu.AlbornozJ.Sprint3.Task7.V14/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.AlbornozJ.Sprint3.Task7.V14.Lib;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int startValue = -5;
+int stopValue = 5;
+
+Console.WriteLine("Старт шага = " + startValue);
+Console.WriteLine("Конец шага = " + stopValue);
+
+int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+double[] valueArray;
+valueArray = new double[len];
+
+valueArray = ds.GetMassFunction(startValue, stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("+-------+--------+");
+Console.WriteLine("|   x   |  f(x)  |");
+Console.WriteLine("+-------+--------+");
+
+for (int i = 0; i <= len - 1; i++)
+{
+    Console.WriteLine("|{0,5:d}  | {1,6:f2} |", startValue, valueArray[i]);
+    startValue++;
+}
+
+Console.WriteLine("+-------+--------+");
+Console.ReadKey();
